c746425bd02cf1ff4ba832850c1a03e3884b99f1
unknown <sumitkp333777@gmail.com>
2024-04-29 15:47:53 +0530

    responsive

--- src/components/PassGenrator/PassGen.jsx
+++ src/components/PassGenrator/PassGen.jsx
@@ -1,25 +1,24 @@
 import React, { useCallback, useEffect, useRef, useState } from "react";
 
 const PassGen = () => {
-  const [length, setLength] = useState(8);
+  const [length, setLength] = useState(5);
   const [numberAllowed, setNumberAllowed] = useState(false);
   const [charAllowed, setCharAllowed] = useState(false);
   const [password, setPassword] = useState("");
   let passRef = useRef(null);
-  let btnContent = useRef(0)
-  
+  let btnContent = useRef(0);
+
   const copyToClipboard = useCallback(() => {
-    passRef.current?.select()
+    passRef.current?.select();
     window.navigator.clipboard.writeText(password);
     let flag = 0;
-    if(flag=0){
+    if ((flag = 0)) {
       btnContent.current.innerHTML = "Copy";
-      flag = 1 ;
-    }else{
+      flag = 1;
+    } else {
       btnContent.current.innerHTML = "Copied";
-      flag = 0
     }
-  },[password])
+  }, [password]);
   const passwordGenrator = useCallback(() => {
     let pass = "";
     let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnpqrstuvwxyz";
@@ -34,16 +33,16 @@ const PassGen = () => {
 
   const getpass = () => {
     passwordGenrator();
-  }
+  };
   // useEffect(() => {
   //   passwordGenrator();
   // }, [length, numberAllowed, charAllowed]);
   return (
     <>
-      <h1 className=" mt-5 text-4xl text-center text-orange-500 font-bold">
+      <h1 className="text-5xl xs:pt-10 text-center text-orange-600 font-extrabold">
         Password Genrator
       </h1>
-      <div className="mt-5 w-[500px] mx-auto py-4 px-4 bg-slate-900 border-1 border-slate-500">
+      <div className="mt-5 lg:w-[50%] xs:w-[85%] mx-auto py-4 px-4 bg-slate-800 border-1 border-slate-500">
         <div className="input flex">
           <input
             type="text"
@@ -53,26 +52,29 @@ const PassGen = () => {
             ref={passRef}
             readOnly
           />
-          <button 
-          ref={btnContent}
-          onClick={copyToClipboard}
-          className="bg-purple-500 py-1 px-4 ml-1 rounded-sm text-white capitalize cursor-pointer hover:bg-purple-800">
+          <button
+            ref={btnContent}
+            onClick={copyToClipboard}
+            className="bg-purple-500 py-1 px-4 ml-1 rounded-sm text-white capitalize cursor-pointer hover:bg-purple-800"
+          >
             copy
           </button>
         </div>
-        <div className="bottom mt-4 flex gap-x-3">
+        <div className="bottom mt-4 flex gap-x-3 xs:flex-col sm:flex-row">
           <input
             type="range"
-            min={8}
-            max={20}
+            min={5}
+            max={15}
             className="cursor-pointer"
             value={length}
             onChange={(e) => {
               setLength(e.target.value);
             }}
           />
-          <label className="text-orange-500 font-medium">length : {length}</label>
-          <div className="checlbox flex gap-x-1">
+          <label className="text-orange-500 font-medium">
+            length : {length}
+          </label>
+          <div className="checlbox flex gap-x-1 ">
             <input
               type="checkbox"
               name="char"
@@ -83,7 +85,7 @@ const PassGen = () => {
                 setCharAllowed((prev) => !prev);
               }}
             />
-            <label htmlFor="charallow" className="text-purple-100">
+            <label htmlFor="charallow" className="text-orange-500 mr-2">
               charallowed
             </label>
             <input
@@ -96,16 +98,27 @@ const PassGen = () => {
                 setNumberAllowed((prev) => !prev);
               }}
             />
-            <label htmlFor="numallow" className="text-purple-100">
+            <label htmlFor="numallow" className="text-orange-500">
               Numberallowed
             </label>
           </div>
         </div>
       </div>
       <div className="text-center mt-4">
-        <button 
-        onClick={getpass}
-        className="text-white bg-red-500 py-2 px-5 rounded-sm hover:bg-red-800">Genrate Password</button>
+        <button
+          onClick={getpass}
+          className="text-white bg-purple-700 py-2 px-5 rounded-sm hover:bg-purple-900 mx-2"
+        >
+          Genrate Password
+        </button>
+        <button
+         className="text-white bg-purple-700 py-2 px-5 rounded-sm hover:bg-purple-900 "
+          onClick={() => {
+            window.location.reload();
+          }}
+        >
+          Refresh
+        </button>
       </div>
     </>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,18 @@ export default {
   ],
   theme: {
     extend: {},
+    screens: {
+      'xs': '350px',
+      // => @media (min-width: 576px) { ... }
+      'sm': '576px',
+      // => @media (min-width: 576px) { ... }
+      'md': '768px',
+      // => @media (min-width: 960px) { ... }
+      'lg': '962px',
+      // => @media (min-width: 1440px) { ... }
+      'xl': '1200px',
+      // => @media (min-width: 1440px) { ... }
+    },
   },
   plugins: [],
 }
